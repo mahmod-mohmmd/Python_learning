ef4001b63c0f59575e61f07dcd8dced509b0a135
Mahmoud Mohamed <eng.mhmudmhmd@gmail.com>
2026-06-22 06:09:15 +0300

    my first simple app with python

---
+++ App_1.py
@@ -0,0 +1,19 @@
+# Inputs
+print("Please Enter your data")
+Name = str(input("Name: "))
+University = str(input("University: "))
+College = str(input("College: "))
+Department = str(input("Cepartment: "))
+Year_of_birth = int(input("Year of birth: "))
+Age = int(input("Age: "))
+City = str(input("City: "))
+
+
+# Outputs
+print("Hello,\nMy name is " + Name)
+print("I study in " + University +" University" )
+print("In college of " + College)
+print("and I am in department of " + Department)
+print("I was born in the year " + str(Year_of_birth))
+print("Now i have " + str(Age) +" years")
+print("I am from " + City)
