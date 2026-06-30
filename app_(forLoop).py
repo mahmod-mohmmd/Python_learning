0ad6c1634e4d15b00f2af0e40c5c03eb64e818d2
Mahmoud Mohamed <eng.mhmudmhmd@gmail.com>
2026-06-30 21:01:14 +0300

    simple app on for loop and dictionary

---
+++ app_(forLoop).py
@@ -0,0 +1,15 @@
+# myRange = range(1,10)
+# for num in myRange :
+#     print(num)
+    
+# dictionary
+myskills = {
+    "html" : "90%",
+    "css" : "50%",
+    "js" : "70%",
+    "c++" : "80%",
+    "python" : "92%"
+}
+# myskills['js'] = 70%
+for k in myskills :
+    print(f"My progress in lang {k} is :{myskills[k]} ")
