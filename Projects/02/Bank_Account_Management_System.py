62e9c03f8a27e900a9f2635fbaea1f90cf46fdc8
Mahmoud Mohamed <eng.mhmudmhmd@gmail.com>
2026-07-16 23:59:57 +0300

    Enhance Bank Account Management System functionality and add new features
    Updated user interface and added new features like showing last transaction and changing username.

--- Projects/02/Bank_Account_Management_System.py
+++ Projects/02/Bank_Account_Management_System.py
@@ -1,25 +1,27 @@
 def main():
     while True :
-            print("1. Enter To Program")
-            print("2. Exit")
+            print("1️⃣  Enter To Program")
+            print("2️⃣  Exit")
             choice = input("Choice (1/2): ")
             if choice == "1":
+                numOfUsers = input("Are you want to know number of users?(y/n): ")
+                if numOfUsers in("y" , "Y"):
+                    print(f"The Number of users in our Bank is: {len(users)}.")
                 mainManue()
             elif choice == "2":
-                print("GoodBye!")
+                print("GoodBye 👋")
                 break
             else:
-                print("Invalid choice, try again.")
+                print("❌Invalid choice, try again‼️")
                 
 def mainManue():
         while True:
-            print("\n=============================================")
+            print("=============================================")
             print("========== Welcom To Python Bank ============")
             print("=============================================")
-            print(" Chose one of this choice : ")
-            print("1. Register")
-            print("2. Login")
-            print("3. Exit")
+            print("1️⃣  Register")
+            print("2️⃣  Login")
+            print("3️⃣  Exit")
             choice = input("Choice (1/2/3): ")
             if choice == "1" :
                 Register()
@@ -29,26 +31,28 @@ def mainManue():
                     bankMenue()
                 
             elif choice == "3" :
-                print("You Chose Exit.")
+                print("You Chose Exit")
                 break
             else :
-                print("Invalid choice, try again.")
+                print("❌Invalid choice, try again‼️")
                 
-def bankMenue():
+def bankMenue():         
+    print(f"--------------------------------\nYour Balance is: {users[userName]["Balance"]} EGP💸")
     while True :
         print("================================")
         print("========== Bank Menue ==========")
         print("================================")
-        print(" Chose one of this choice : ")
-        print("1. Check Balance")
-        print("2. Deposit")
-        print("3. Withdraw")
-        print("4. Transfer")
-        print("5. Change Password")
-        print("6. Logout")
-        print("7. Main Menue")
+        print("1️⃣  Check Balance")
+        print("2️⃣  Deposit💸")
+        print("3️⃣  Withdraw💸")
+        print("4️⃣  Transfer💱")
+        print("5️⃣  Change Password")         
+        print("6️⃣  Change User Name")
+        print("7️⃣  Logout")
+        print("8️⃣  Show Last Transacion")
+        print("9️⃣  Main Menue")
         
-        choice = input("Choice (1/2/3/4/5/6/7): ")
+        choice = input("Choice (1/2/3/4/5/6/7/8/9): ")
         if choice == "1":
             checkBalance()
         elif choice == '2':
@@ -60,16 +64,40 @@ def bankMenue():
         elif choice == '5':
             changePassword() 
         elif choice == '6':
+            changeUsername() 
+        elif choice == '7': 
             logout() 
             break
-        elif choice == '7': 
+        elif choice == '8': 
+            showLastTransaction() 
+        elif choice == '9': 
             mainManue() 
         else :
-            print("Invalid choice, try again.")
+            print("❌Invalid choice, try again‼️")
             
+            
+def showLastTransaction():
+    print(users[userName]["lastTransaction"])
+    
+def printReceipt(transacionType , amount , recipient = ""):
+    
+    print("================================================")
+    print(f"------------------🏦 Receipt🧾------------------")
+    # print("--------- Python Bank 🏦 ---------")
+    print("================================================")
+    print(f"Current User Name : {userName}")
+    print(f"Transacion Type   : {transacionType}")
+    print(f"The Amount used   : {amount} EGP💸")
+    if recipient != "":
+        print(f"Transfered To : {recipient}")
+    
+    print(f"Remaining Balance : {users[userName]["Balance"]} EGP💸")
+    print("===============================================")
+    print("     ❤️  Thank You For Using Our Bank 🏦     ")
+    print("===============================================\n")
+    
 users = {}
 userName = ""
-
 def Register():
     global userName
     print("You In Register.")
@@ -77,19 +105,20 @@ def Register():
     userName = input("Please Enter Your Name: ").strip().capitalize()
     
     while userName in users   :
-        print("User Name Exists!")
+        print("User Name Exists‼️")
         userName = input("Please Enter Another Name: ").strip().capitalize()
     
     password = (input("Please Enter Your Password: "))
     
     while len(password) < 6  or password == "" :
-        print("Password invalid , Password Must be AtLeast 6!!")
+        print("❌Password invalid,Password Must be AtLeast 6‼️")
         password = (input("Enter Your Password Again:  "))
         
     users[userName] = {}
-    users[userName] = {"Password" : password , "Balance" : 0}
+    users[userName] = {"Password" : password , "Balance" : 0, "lastTransaction":"No Transaction yet"}
     
-    print("Registration Successful!")
+    print("Registration Successfull✅")
+    print(f"Now The Number of users in our Bank Is: {len(users)}")
     print("Go To The Main Menue...") 
     return
 
@@ -106,15 +135,15 @@ def Login() :
             print(f"Error , You Have Now {attempts} Try!") 
             ask = input("Are You New Here? (y/n): ")
             if ask in("y" , "Y"):
-                print("The account does not exist. Please go to register.")
+                print("❌The account does not exist. Please go to register‼️")
                 return False
                 
             else :
                 print(f"Error , You Have Now {attempts} Try!") 
-                print("You are write wrong User Name or password, Please Register First.")
+                print("❌You are write wrong User Name or password, Please Register First‼️")
                 
         elif (users[userName]["Password"] == password) :
-            print("Login Successfull!") 
+            print("Login Successfully✅") 
             print("Go To The Bank Menue...") 
             return True
     
@@ -132,20 +161,26 @@ def deposit():
     
     print(f"Your Balance Befor Deposit is {users[userName]["Balance"]}")
     users[userName]["Balance"]+= dAmount 
-    print("Deposit Successfull!")
-    print(f"Your Balance Now is {users[userName]["Balance"]}")
-        
+    users[userName]["lastTransaction"] = f"\nDeposit {dAmount} EGP💸\n"
+    print("Deposit Successfull✅")
+    # print(f"Your Balance Now is {users[userName]["Balance"]}")
+    
+    printReceipt("Deposit", dAmount)
+    
 
 def withdraw() :
     wAmount = float(input("Plealse Enter Your Amount You Want To Withdraw it: "))
     while wAmount <= 0 or wAmount > users[userName]["Balance"] :
-        print("AThe amount is invalid or your balance is insufficient.")
+        print("❌The amount is invalid or your balance is insufficient‼️")
         wAmount = float(input("Please Enter Sufficient Amount  : "))
     
     print(f"Your Balance Befor Withdraw is {users[userName]["Balance"]}")
     users[userName]["Balance"]-= wAmount 
-    print("Withdraw Successfull!")
-    print(f"Your Balance Now is {users[userName]["Balance"]}")
+    users[userName]["lastTransaction"] = f"\nWithdrew {wAmount} EGP💸\n"
+    print("Withdraw Successfull✅")
+    # print(f"Your Balance Now is {users[userName]["Balance"]}")
+    
+    printReceipt("Withdraw", wAmount)
 
 def transfer() :
     global userName
@@ -164,21 +199,24 @@ def transfer() :
         tAmount = float(input("Plealse Enter Your Amount You Want To Transfer it: "))
         if tAmount > 0 and (users[userName]["Balance"] >= tAmount):
             
-            print("==========================================================================")
+            print("===============================================")
             print(f"BEFOR Transfer user: {userName} Has Balance  {users[userName]["Balance"]}")
-            print(f"BEFOR Transfer user: {recipientName} Has Balance {users[recipientName]["Balance"]}")
-            print("==========================================================================")
+            print(f"Now AFTER Transfer user: {userName} Has Balance {users[userName]["Balance"]}")
+            # print("===============================================")
                         
             users[userName]["Balance"]-= tAmount 
             users[recipientName]["Balance"] += tAmount
-            
-            print("==========================================================================")
-            print(f"Now AFTER Transfer user: {userName} Has Balance {users[userName]["Balance"]}")
+            users[userName]["lastTransaction"] = f"\bTransfered {tAmount} EGP💸\n"
+                                                                  
+            print("----------------------------------------------")
+            print(f"BEFOR Transfer user: {recipientName} Has Balance {users[recipientName]["Balance"]}")
             print(f"Now AFTER Transfer user: {recipientName} Has Balance {users[recipientName]["Balance"]}")
-            print("==========================================================================")
+            print("===============================================")
             
     else :
-        print("You cannot transfer to yourself")
+        print("❌You cannot transfer to yourself‼️")
+    printReceipt("Transfer", tAmount, recipient=recipientName)
+    
 
 def changePassword():
     password = (input("Please Enter Your old Password: "))
@@ -188,10 +226,26 @@ def changePassword():
             print("Password invalid , Password Must be AtLeast 6!!")
             newPassword = (input("Enter Your Password Again:  "))
         users[userName]["Password"] =  newPassword 
-        print("Password Change Successfull!")
-        # print("Go to login page To sure for New Password...\n")
-        return    
+        print("Password Changed Successfully✅")
+        return 
+    
+def changeUsername():
+    global userName 
+    oldName = userName
+    
+    print(f"Your Current username is : {oldName}")
+    newName = (input("Please Enter Your New Name: ")).strip().capitalize()
+    while newName in users   :
+        print("User Name Exists‼️")
+        newName = input("Please Enter Another Name: ").strip().capitalize()
+    users[newName] = users[oldName]
+    del users[oldName]
+    userName = newName
+    print("UserName Changed Successfully✅")
+    return
+    
 def logout():
     global userName 
     userName = ""
+    
 main()
